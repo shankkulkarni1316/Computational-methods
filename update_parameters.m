932c9bc9a5a306d94bd632547106fd70807665c7
Shank S. Kulkarni <53656125+shankkulkarni1316@users.noreply.github.com>
2019-08-03 11:04:51 -0400

    Delete update_parameters.m

--- update_parameters.m
+++
@@ -1,9 +0,0 @@
-function [u_past, u]=update_parameters(A,B)
-% This function updates values of displacement for next time step as
-% follows ----->   
-% u^(n+1) becomes u^(n)
-% u^(n) becomes u^(n-1)
-
-    u_past = A;
-    u = B;
-end
